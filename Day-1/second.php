fa645002b6b9e57d1ccb069e50b4f12e6d115f47
Molnar Laszlo <laszlo.molnar25@gmail.com>
2019-06-17 11:49:56 +0200

    Test include

--- Day-1/second.php
+++ Day-1/second.php
@@ -1,28 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
-
 <body>
-
     <?php
-
-    echo "Hola Jorge" . "<br>";
-
-    $arr = [123, -123, 0x1A, 0b10010, 0123];
-
-    var_dump($arr);
-    
-    include "second.php";
-
-
-    ?>
-
+    echo "<br>this is from the second one";
+?>
 </body>
-
 </html>
