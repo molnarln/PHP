98afa61a6068149140862cf07e87cc6a08f752dc
Molnar Laszlo <laszlo.molnar25@gmail.com>
2019-06-17 10:18:10 +0200

    Initial commit

---
+++ Day-1/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+<?php 
+
+echo "Hola Jorge";
+
+$arr = [123, -123, 0x1A, 0b10010, 0123];
+
+?>
+    
+</body>
+</html>
